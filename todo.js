fc032292114593c13496b90f3b5769000c527007
IIngaV <inga1996@tut.by>
2017-11-13 20:24:52 +0300

    todolist

--- todo.js
+++ todo.js
@@ -1,15 +1,12 @@
 var deal=2;
 check();
 made();
-
+deleted();
 function check(){
 	var list = document.getElementsByTagName("li");
 	list.className="list-group-item";
 	for (var i = 0; i < list.length; i++) 
 	{
-		  /*var span = document.createElement("span");
-		  var txt = document.createElement("input");
-		  txt.type = "checkbox";*/
 		  var btn = document.createElement("BUTTON");
 		  var t = document.createTextNode("Delete");
 		  btn.appendChild(t);
@@ -22,42 +19,42 @@ function check(){
 		  btn1.className = "made";
 		  count();
 		  made();
+		  deleted();
 	}
 }
-
-function todoList() {
-	var item = document.getElementById("todoInput").value;
-	var text = document.createTextNode(item);
-	var newItem = document.createElement("li");
-	newItem.className="list-group-item";
-	if (text.length>0){
-	newItem.appendChild(text);
-	if (document.getElementById("importantCB").checked){
-		document.getElementById("todoList").appendChild(newItem).style.backgroundColor ="#b94e48"; 	
+	var save = document.getElementById("save");
+	save.onclick = function(){
+		var item = document.getElementById("todoInput").value;
+		var text = document.createTextNode(item);
+		var newItem = document.createElement("li");
+		newItem.className="list-group-item";
+		if (text.length>0){
+			var add=document.getElementById("todoList").appendChild(newItem);	
+			newItem.appendChild(text);
+			if (document.getElementById("importantCB").checked){
+				add.style.backgroundColor ="#b94e48"; 	
+			}
+			else{
+				add;	
+			}
+			deal++;
+			check();
+			count();
+		}
+		else{
+			alert("Дело не может быть пустым");
+		}
+		document.getElementById("todoInput").value=null;
 	}
-	else{
-		document.getElementById("todoList").appendChild(newItem);
-		
-	}
-	deal++;
-	check();
-	count();
-	}
-	else{
-	alert("Дело не может быть пустым");
-	}
-	document.getElementById("count").value = deal;
-	 document.getElementById("todoInput").value=null;
-	 
-	}
+	
 function deleted(){
-var list = document.getElementsByClassName("check");
-	for (var i = 0; i < list.length; i++) {
-		list[i].onclick = function() {
-		var del = this.parentElement;
-    	del.remove();
-    	deal--;
-    	count();
+var ch = document.getElementsByClassName("check");
+	for (var i = 0; i < ch.length; i++) {
+		ch[i].onclick = function() {
+			var del = this.parentElement;
+    		del.remove();
+    		deal--;
+    		count();
 		}
 	}
 }
@@ -65,8 +62,8 @@ function made(){
 var list = document.getElementsByClassName("made");
 	for (var i = 0; i < list.length; i++) {
 		list[i].onclick = function() {
-		var del = this.parentElement;
-		del.style.backgroundColor ="#00a550";
+			var del = this.parentElement;
+			del.style.backgroundColor ="#00a550";
 		}
 	}
 }
@@ -74,5 +71,4 @@ function count(){
 	document.getElementById("c").innerHTML = deal;
 }
 count();
-deleted();
 
